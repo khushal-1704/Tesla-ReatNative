73d9ff77fd6d9d5cea17820397e1a8b07333333c
Khushal <khushalksain@gmail.com>
2022-03-11 23:16:00 +0545

    [TC-11-March] Add Custom Order Navigation

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { View, Text, FlatList, Dimensions, } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import CarItem from "../CarItem";
-
 import styles from './styles';
 import cars from './cars';
+import Header from '../Header';
 
-const CarsList = (props) => {
+
+
+const CarsList = ({ navigation }) => {
     return (
         <View style={styles.container}>
+            <Header />
             <FlatList
                 data={cars}
                 renderItem={({ item }) => <CarItem car={item} />}
                 snapToAlignment={'end'}
                 decelerationRate={'normal'}
-
             />
         </View>
     );
--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -3,9 +3,6 @@ import { Text, View, Pressable } from 'react-native'
 import styles from './styles'
 
 const StyleButton = ({ type, content, onPress }) => {
-
-
-
     const backgroundColor = type === 'primary' ? '#171A20CC' : '#FFFFFFA6';
     const textColor = type === 'primary' ? '#FFFFFF' : '#171A20CC';
 
@@ -13,9 +10,7 @@ const StyleButton = ({ type, content, onPress }) => {
         <View style={styles.container}>
             <Pressable
                 style={[styles.button, { backgroundColor: backgroundColor }]}
-                onPress={() => {
-                    onPress()
-                }}>
+                onPress={onPress}>
                 <Text style={[styles.text, { color: textColor }]}>{content}</Text>
             </Pressable>
         </View>
